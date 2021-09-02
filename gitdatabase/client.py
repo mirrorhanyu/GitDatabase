1e3be93ca3442ffb32d20fc15d8180affcd72636
mirrorhanyu <mirrorhanyu@gmail.com>
2021-09-02 13:21:04 +0800

    use access token instead of username and password

--- gitdatabase/client.py
+++ gitdatabase/client.py
@@ -4,14 +4,15 @@ import shutil
 from git import Repo
 
 from gitdatabase.collection import Collection
+from random import random
 
 
 class Client:
-    def __init__(self, repo, username, password, **kwargs):
+    def __init__(self, repo, access_token, **kwargs):
         branch = kwargs.pop('branch', 'main')
-        local_path = f'{os.path.dirname(os.path.dirname(__file__))}/{username}-database'
+        local_path = f'{os.path.dirname(os.path.dirname(__file__))}/database-{random()}'
         shutil.rmtree(local_path, ignore_errors=True)
-        remote = f'https://{username}:{password}@{repo}'
+        remote = f'https://{access_token}:x-oauth-basic@{repo}'
         Repo.clone_from(remote, local_path, branch=kwargs.pop('branch', 'main'))
         self.database = Repo(local_path)
 
